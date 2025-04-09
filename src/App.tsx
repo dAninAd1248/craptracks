c74fc7311d0aad359afe5aaa6bdf53b59b8b17d5
dAninAd1248 <124212726+dAninAd1248@users.noreply.github.com>
2025-04-08 21:14:41 -0400

    Message

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        TEEEEST
+      <p>
+        This is a website where you can post music tracks that you made.
+        <br/>
+        But.
+        <br/>
+        They have to be 💩.
+        <br/>
+        ⚠️ UNDER CONSTRUCTION ⚠️
       </p>
     </>
   )
